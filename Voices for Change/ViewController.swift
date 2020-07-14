821046a791b5b676bb79038c8f038f225e612884
Caitlyn Widjaja <widjajacaitlyn@gmail.com>
2020-07-14 13:21:35 -0700

    Initial Commit

---
+++ Voices for Change/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Voices for Change
+//
+//  Created by Caitlyn Widjaja on 7/14/20.
+//  Copyright © 2020 Caitlyn Widjaja. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
