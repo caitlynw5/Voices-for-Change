23160a49125ec2a6008fbb192b4323a6cdfc0eda
Caitlyn Widjaja <widjajacaitlyn@gmail.com>
2020-07-15 23:14:05 -0700

    Caitlyn - updated Human Rights resource links

--- Voices for Change/HumRightsResourcesViewController.swift
+++ Voices for Change/HumRightsResourcesViewController.swift
@@ -10,11 +10,6 @@ import UIKit
 
 class HumRightsResourcesViewController: UIViewController {
 
-    @IBAction func allYouNeedToKnow(_ sender: AnyObject) {
-        if let url = URL (string: "https://www.bbc.com/news/world-asia-china-45474279") {
-            UIApplication.shared.open(url)
-        }
-    }
     @IBOutlet weak var topicLabel: UILabel!
     
     @IBOutlet weak var resourceLabel: UILabel!
@@ -23,17 +18,95 @@ class HumRightsResourcesViewController: UIViewController {
     
     @IBOutlet weak var subHead1: UILabel!
     
+    @IBAction func subHead1Link1(_ sender: AnyObject) {
+        if let url = URL (string: "https://www.bbc.com/news/world-asia-china-45474279") {
+            UIApplication.shared.open(url)
+        }
+    }
+    
+    @IBAction func subHead1Link2(_ sender: AnyObject) {
+        if let url = URL (string: "https://www.bbc.com/news/world-asia-china-26414014") {
+            UIApplication.shared.open(url)
+        }
+    }
+    
+    @IBAction func subHead1Link3(_ sender: AnyObject) {
+        if let url = URL (string: "https://www.theguardian.com/news/2018/aug/07/why-uighur-muslims-across-china-are-living-in-fear") {
+             UIApplication.shared.open(url)
+         }
+    }
     
     @IBOutlet weak var subHead2: UILabel!
     
+    @IBAction func subHead2Link1(_ sender: AnyObject) {
+        if let url = URL (string: "https://www.hrc.org/resources/violence-against-the-trans-and-gender-non-conforming-community-in-2020") {
+             UIApplication.shared.open(url)
+         }
+    }
+    
+    @IBAction func subHead2Link2(_ sender: AnyObject) {
+        if let url = URL (string: "https://www.newyorker.com/news/our-columnists/the-lgbt-refugees-in-turkey-who-refuse-to-be-forgotten") {
+             UIApplication.shared.open(url)
+         }
+    }
     
     @IBOutlet weak var subHead3: UILabel!
     
     
+    @IBAction func subHead3Link1(_ sender: AnyObject) {
+        if let url = URL (string: "https://www.unicef.org/emergencies/yemen-crisis") {
+             UIApplication.shared.open(url)
+         }
+    }
+    
     @IBOutlet weak var instaDivider: UILabel!
     
-
+    @IBAction func instaAcc1(_ sender: AnyObject) {
+        if let url = URL (string: "https://www.instagram.com/humanrightscampaign/") {
+             UIApplication.shared.open(url)
+         }
+    }
+    
+    @IBAction func instaAcc2(_ sender: AnyObject) {
+        if let url = URL (string: "https://www.instagram.com/humanrightswatch/") {
+             UIApplication.shared.open(url)
+         }
+    }
+    
+    
+    @IBAction func instaAcc3(_ sender: AnyObject) {
+        if let url = URL (string: "https://www.instagram.com/unwomen/") {
+             UIApplication.shared.open(url)
+         }
+    }
+    
+    @IBAction func instaAcc4(_ sender: AnyObject) {
+        if let url = URL (string: "https://www.instagram.com/amnesty/") {
+             UIApplication.shared.open(url)
+         }
+    }
+    
     @IBOutlet weak var podcastDivider: UILabel!
+    
+    @IBAction func podcast1(_ sender: AnyObject) {
+        if let url = URL (string: "https://anchor.fm/declarations") {
+             UIApplication.shared.open(url)
+         }
+    }
+    
+    @IBAction func podcast2(_ sender: AnyObject) {
+        if let url = URL (string: "http://www.thehumpodcast.com/") {
+             UIApplication.shared.open(url)
+         }
+    }
+    
+    @IBAction func podcast3(_ sender: AnyObject) {
+        if let url = URL (string: "http://www.rightstrack.org/") {
+             UIApplication.shared.open(url)
+         }
+    }
+    
+    
     
     
     override func viewDidLoad() {
