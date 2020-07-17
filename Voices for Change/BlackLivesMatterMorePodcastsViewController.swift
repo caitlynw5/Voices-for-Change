42d2ac5fae981777c518956dfe304ac0dd132bfa
linishere <miyamoto.linaly@gmail.com>
2020-07-16 19:53:43 -0700

    finished

--- Voices for Change/BlackLivesMatterMorePodcastsViewController.swift
+++ Voices for Change/BlackLivesMatterMorePodcastsViewController.swift
@@ -27,7 +27,37 @@ class BlackLivesMatterMorePodcastsViewController: UIViewController {
     @IBAction func sixNine(_ sender: AnyObject) {
         if let url = URL(string: "https://www.nytimes.com/2019/08/23/podcasts/1619-slavery-anniversary.html"){UIApplication.shared.open(url)}
     }
+    @IBAction func activistsShorts(_ sender: AnyObject) {
+        if let url = URL(string:"https://blacklivesmatter.com/activist-shorts/"){UIApplication.shared.open(url)}
+    }
     
+    @IBAction func bcaHeritage(_ sender: AnyObject) {
+        if let url = URL(string: "https://www.instagram.com/bcaheritage/"){UIApplication.shared.open(url)}
+    }
+    @IBAction func visionsCollective(_ sender: AnyObject) {
+        if let url = URL(string: "https://www.instagram.com/blackvisionscollective/"){UIApplication.shared.open(url)}
+    }
+    @IBAction func ckPrivilege(_ sender: AnyObject) {
+        if let url = URL(string: "https://www.instagram.com/ckyourprivilege/?utm_source=ig_embed"){UIApplication.shared.open(url)}
+    }
+    @IBAction func aclu(_ sender: AnyObject) {
+        if let url = URL(string: "https://www.instagram.com/aclu_nationwide/?utm_source=ig_embed"){UIApplication.shared.open(url)}
+    }
+    @IBAction func blkLivesMatter(_ sender: AnyObject) {
+        if let url = URL(string: "https://www.instagram.com/blklivesmatter/?hl=en"){UIApplication.shared.open(url)}
+    }
+    @IBAction func blmPolice(_ sender: AnyObject) {
+        if let url = URL(string: "https://www.youtube.com/watch?v=S6rDvbQ-mz4"){UIApplication.shared.open(url)}
+    }
+    @IBAction func openMic(_ sender: AnyObject) {
+        if let url = URL(string: "https://www.youtube.com/watch?v=tmtBojl0Y90"){UIApplication.shared.open(url)}
+    }
+    @IBAction func aroundTheWorld(_ sender: AnyObject) {
+        if let url = URL(string: "https://www.youtube.com/watch?v=4Vl4I0weXPU"){UIApplication.shared.open(url)}
+    }
+    @IBAction func protestersPolice(_ sender: AnyObject) {
+        if let url = URL(string: "https://www.youtube.com/watch?v=24xsqyMcpRg&t=1s"){UIApplication.shared.open(url)}
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
 
